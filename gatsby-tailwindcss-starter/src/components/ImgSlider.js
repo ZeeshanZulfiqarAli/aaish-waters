95afc78f2bec4fea1667f15f31ea71e5fac718b7
ZeeshanZulfiqarAli <zeeshan.dossani23@gmail.com>
2021-10-04 00:00:52 +0500

    Added image slider

---
+++ gatsby-tailwindcss-starter/src/components/ImgSlider.js
@@ -0,0 +1,53 @@
+import Slider from "react-slick";
+import { Link } from "gatsby"
+import logo from "../../assets/media/logo.png"
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import React from "react"
+
+const images = [
+  {
+    src: "https://culligan.com.pk/wp-content/uploads/2021/06/8.jpg",
+    alt: "sample image"
+  },
+  {
+    src: "https://culligan.com.pk/wp-content/uploads/2021/06/4.jpg",
+    alt: "sample image"
+  },
+  {
+    src: "https://culligan.com.pk/wp-content/uploads/2021/06/1.jpg",
+    alt: "sample image"
+  },
+  {
+    src: "https://culligan.com.pk/wp-content/uploads/2021/06/2.jpg",
+    alt: "sample image"
+  }
+];
+
+function ImgSlider() {
+  const settings = {
+      dots: true,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 5000,
+      cssEase: "ease-in-out"
+    };
+  return (
+    <section className="relative w-screen" style={{left: "calc(100% - 100vw + (100vw - 100%) / 2)"}}>
+      <Slider {...settings}>
+        {
+          images.map((image)=>(
+            <img src={image.src} alt={image.alt}/>
+          ))
+        }
+      </Slider>
+    </section>
+  )
+}
+
+export default ImgSlider
