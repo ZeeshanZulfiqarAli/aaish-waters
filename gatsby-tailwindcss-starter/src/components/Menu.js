d119a8e78a29ff11f3fe5f3ab11b45998bb1f4f6
ZeeshanZulfiqarAli <zeeshan.dossani23@gmail.com>
2021-10-04 00:03:25 +0500

    Added menu items

--- gatsby-tailwindcss-starter/src/components/Menu.js
+++ gatsby-tailwindcss-starter/src/components/Menu.js
@@ -1,12 +1,17 @@
 import { Link } from "gatsby"
-import React, { useState } from "react"
+import logo from "../../assets/media/logo.png"
+
+import React from "react"
 
 function Menu() {
 
   return (
-    <nav >
-        <Link >Certifications</Link>
-        <Link>Services</Link>
+    <nav className="h-24 flex my-10 place-content-between">
+        <img src={logo} className="h-full"/>
+        <div className=" self-center">
+          <Link to={"#cert"} className="mx-2 border-b-4 border-indigo-600 border-opacity-0 hover:border-opacity-100" activeClassName="border-b-4" activeStyle={{ color: "red" }}>Certifications</Link>
+          <Link to={"#serv"} className="mx-2 border-b-4 border-indigo-600 border-opacity-0 hover:border-opacity-100" activeClassName="border-b-4">Services</Link>
+        </div>
     </nav>
   )
 }
