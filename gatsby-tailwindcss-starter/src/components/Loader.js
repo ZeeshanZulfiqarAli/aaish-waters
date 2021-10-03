372a024a7b56d8202f65be4fb6a60e3d600fe2c3
ZeeshanZulfiqarAli <zeeshan.dossani23@gmail.com>
2021-10-04 00:02:46 +0500

    Increased loader bottom blur height

--- gatsby-tailwindcss-starter/src/components/Loader.js
+++ gatsby-tailwindcss-starter/src/components/Loader.js
@@ -17,7 +17,7 @@ function Loader({ delay }) {
     <div className={classnames("fixed left-0 top-0 h-full w-full z-30 transition-transform duration-1000",{"transform -translate-y-full": remove})}>
       {/* <div className="object-cover"> */}
         <video className="absolute object-cover h-full w-full  " src={water} autoPlay muted loop/>
-        <div className={classnames("absolute -bottom-20 w-full h-40 backdrop-filter backdrop-blur-lg transition-transform delay-1000", {"transform -translate-y-full": remove})}/>
+        <div className={classnames("absolute -bottom-1/4 w-full h-1/2 backdrop-filter backdrop-blur-3xl transition-transform delay-1000", {"transform -translate-y-full": remove})}/>
       {/* </div> */}
 
       <img src={logo} alt="site logo" className="absolute w-full -top-14 h-full p-20 sm:p-40 object-scale-down"/>
